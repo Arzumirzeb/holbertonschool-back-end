c7b75558e08ed394778ac9c5817982bbc3963278
arzumirzeb <arzumirzeb03@gmail.com>
2024-04-23 21:30:25 +0400

    hotfix:(

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-"""Script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress"""
+"""Script that, using this REST API,
+for a given employee ID, returns information about
+his/her TODO list progress"""
 
 
 import requests
@@ -8,14 +10,14 @@ from sys import argv
 
 
 def fetch_employee_todo_progress(employee_id):
-    user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    user_response = requests.get(f"https://jsonplaceholder.\
+            typicode.com/users/{employee_id}")
     user_data = user_response.json()
     employee_name = user_data['name']
 
-
-    todos_response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+    todos_response = requests.get(f"https://jsonplaceholder.\
+            typicode.com/todos?userId={employee_id}")
     todos_data = todos_response.json()
-
 
     completed_tasks = []
     for task in todos_data:
@@ -24,8 +26,8 @@ def fetch_employee_todo_progress(employee_id):
     num_completed_tasks = len(completed_tasks)
     total_tasks = len(todos_data)
 
-
-    print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):")
+    print(f"Employee {employee_name} is done with tasks\
+            ({num_completed_tasks}/{total_tasks}):")
     for task in completed_tasks:
         print(f"\t{task['title']}")
 
@@ -33,7 +35,6 @@ def fetch_employee_todo_progress(employee_id):
 if __name__ == "__main__":
     if len(argv) != 2:
         print("Usage: python script.py <employee_id>")
-
 
     employee_id = argv[1]
     fetch_employee_todo_progress(employee_id)
